80a9de83fc51e35c0cbf280adcb7fc9763e524b8
Miguel Borges <miguel.borges@hotmail.com>
2019-03-12 12:36:14 +0000

    Update to code.

---
+++ scripts/solveneural4.py
@@ -0,0 +1,157 @@
+import sys
+import cv2
+import pickle
+import libsolve
+import libnrvsolve
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+def build_model():
+  model = keras.Sequential([
+    layers.Dense(4, activation=tf.nn.sigmoid, input_shape=(4,)),
+    layers.Dense(4, activation=tf.nn.sigmoid),
+    layers.Dense(4, activation=tf.nn.sigmoid),
+    layers.Dense(4, activation=tf.nn.sigmoid),
+    layers.Dense(1)
+  ])
+
+  optimizer = tf.keras.optimizers.SGD(0.001)
+
+  model.compile(loss='mse',
+                optimizer=optimizer,
+                metrics=['mae', 'mse'])
+  return model
+
+def defaultprint():
+  print("Usage:")
+  print("python solveneural4.py -train [<trainfile.bag> ...]  - test [<testfile.bag> ...]")
+  print("python solveneural4.py -test [<testfile.bag> ...]")
+  sys.exit(0)
+
+def main(args):
+  trainfiles = list()
+  testfiles = list()
+  load = False
+  save = False
+
+  # Reading arguments
+  index = 0
+  while index < len(args):
+    if args[index] == "-h":
+      defaultprint()
+    if args[index] == "-s":
+      save = True
+    if args[index] == "-train":
+      index += 1
+      while (index < len(args)
+        and args[index] != "-test"
+        and args[index] != "-train"
+        and args[index] != "-test"):
+        trainfiles.append(args[index])
+        index += 1
+      index -= 1
+    if args[index] == "-test":
+      index += 1
+      while (index < len(args)
+        and args[index] != "-test"
+        and args[index] != "-train"
+        and args[index] != "-test"):
+        testfiles.append(args[index])
+        index += 1
+      index -= 1
+    index += 1
+
+  if len(testfiles) == 0:
+    defaultprint()
+
+  if len(trainfiles) == 0:
+    load = True
+
+  # Reading tracker data
+  hmodel = None
+  vmodel = None
+  if not load:
+    data = [np.empty([0,4]),
+      np.empty([0,1]),
+      np.empty([0,4]),
+      np.empty([0,1])]
+    for trainfile in trainfiles:
+      trackers = dict()
+      br = libsolve.BagReader(trainfile)
+      br.read("/loc/vive/trackers", libnrvsolve.trackerhandler, trackers)
+      # Reading light data
+      lr = libnrvsolve.LightReader(trainfile, trackers)
+      lr.read("/loc/vive/light", data)
+
+    # Undersampling
+    SPLIT = 0.5
+    h_idx = np.random.choice(range(data[0].shape[0]),
+      int(data[0].shape[0] * SPLIT),
+      replace=False)
+    v_idx = np.random.choice(range(data[2].shape[0]),
+      int(data[2].shape[0] * SPLIT),
+      replace=False)
+
+    # Model
+    hmodel = build_model()
+    vmodel = build_model()
+
+    EPOCHS = 2000
+    hmodel.fit(data[0][h_idx], data[1][h_idx],
+      epochs=EPOCHS, validation_split = 0.0,
+      verbose=True, batch_size=100, shuffle = True)
+    vmodel.fit(data[2][v_idx], data[3][v_idx],
+      epochs=EPOCHS, validation_split = 0.0,
+      verbose=True, batch_size=100, shuffle = True)
+    models = (hmodel,vmodel)
+
+    # Saving the data
+    if save:
+      dumpfile = open("tmp/svr_models4.bin", "wb")
+      pickle.dump(models, dumpfile)
+      dumpfile.close()
+  else:
+    # Saving the data
+    loadfile = open("tmp/svr_models4.bin", "rb")
+    models = pickle.load(loadfile)
+    loadfile.close()
+
+    hmodel = models[0]
+    vmodel = models[1]
+
+  # Reading Testing data
+  data = [np.empty([0,4]),
+    np.empty([0,1]),
+    np.empty([0,4]),
+    np.empty([0,1])]
+  for testfile in testfiles:
+    trackers = dict()
+    br = libsolve.BagReader(testfile)
+    br.read("/loc/vive/trackers", libnrvsolve.trackerhandler, trackers)
+    # Reading light data
+    lr = libnrvsolve.LightReader(testfile, trackers)
+    lr.read("/loc/vive/light", data)
+
+  # Predict with test data
+  predicted_hdata = hmodel.predict(data[0])
+  predicted_vdata = vmodel.predict(data[2])
+
+  # Plot
+  fig, axs = plt.subplots(2, 2, sharex = False, sharey= False)
+  axs[0,0].plot(predicted_hdata)
+  axs[0,0].plot(data[1])
+  axs[0,1].plot(predicted_vdata)
+  axs[0,1].plot(data[3])
+  hist, bin_edges = np.histogram(predicted_hdata - data[1], bins = 20)
+  axs[1,0].bar(bin_edges[:-1], hist, width = bin_edges[0] - bin_edges[1])
+  hist, bin_edges = np.histogram(predicted_vdata - data[3], bins = 20)
+  axs[1,1].bar(bin_edges[:-1], hist, width = bin_edges[0] - bin_edges[1])
+  plt.show()
+
+if __name__ == '__main__':
+  main(sys.argv)
--- scripts/solvesvr4.py
+++ scripts/solvesvr4.py
@@ -2,7 +2,7 @@ import sys
 import cv2
 import pickle
 import libsolve
-import libvsolve
+import libnrvsolve
 import numpy as np
 from sklearn.svm import SVR
 import matplotlib.pyplot as plt
@@ -38,18 +38,73 @@ def main(args):
   if not load:
     trackers = dict()
     br = libsolve.BagReader(trainfile)
-    br.read("/loc/vive/trackers", libvsolve.trackerhandler, trackers)
+    br.read("/loc/vive/trackers", libnrvsolve.trackerhandler, trackers)
     # Reading light data
-    data = [np.empty([0,6]),
+    data = [np.empty([0,4]),
       np.empty([0,1]),
-      np.empty([0,6]),
+      np.empty([0,4]),
       np.empty([0,1])]
-    lr = libvsolve.LightReader(trainfile, trackers)
+    lr = libnrvsolve.LightReader(trainfile, trackers)
     lr.read("/loc/vive/light", data)
+
+    # # Coordinate conversion
+    # reduced_h = np.empty([0,4])
+    # reduced_v = np.empty([0,4])
+    # for i in range(0,data[0].shape[0]):
+    #   distance = np.sqrt(data[0][i,0]**2 + data[0][i,1]**2)
+    #   alpha_h = np.arctan2(data[0][i,0] , data[0][i,2])
+    #   height = data[0][i,1]
+    #   Ry = np.matrix([[np.cos(alpha_h),0.0,np.sin(alpha_h)],
+    #     [0.0, 1.0, 0.0],
+    #     [-np.sin(alpha_h),0.0,np.cos(alpha_h)]])
+    #   R = cv2.Rodrigues(data[0][i,3:6])[0]
+    #   new_R = Ry * R
+    #   trAl = cv2.Rodrigues(new_R)[0]
+    #   orientation_x = trAl[0]
+    #   orientation_y = trAl[1]
+    #   orientation_z = trAl[2]
+    #   features = np.array([distance, orientation_x, orientation_y, orientation_z])
+    #   reduced_h = np.vstack((reduced_h, features))
+
+    # for i in range(0,data[2].shape[0]):
+    #   distance = np.sqrt(data[2][i,0]**2 + data[2][i,1]**2)
+    #   alpha_v = np.arctan2(data[2][i,1] , data[2][i,2])
+    #   height = data[2][i,0]
+    #   Ry = np.matrix([[np.cos(alpha_v),0.0,np.sin(alpha_v)],
+    #     [0.0, 1.0, 0.0],
+    #     [-np.sin(alpha_v),0.0,np.cos(alpha_v)]])
+    #   R = cv2.Rodrigues(data[2][i,3:6])[0]
+    #   new_R = Ry * R
+    #   trAl = cv2.Rodrigues(new_R)[0]
+    #   orientation_x = trAl[0]
+    #   orientation_y = trAl[1]
+    #   orientation_z = trAl[2]
+    #   features = np.array([distance, orientation_x, orientation_y, orientation_z])
+    #   reduced_v = np.vstack((reduced_v, features))
+
+    # # Plot
+    # fig, axs = plt.subplots(6, 3, sharex = False, sharey= False)
+    # axs[0,0].plot(data[0][:,0])
+    # axs[1,0].plot(data[0][:,1])
+    # axs[2,0].plot(data[0][:,2])
+    # axs[3,0].plot(data[0][:,3])
+    # axs[4,0].plot(data[0][:,4])
+    # axs[5,0].plot(data[0][:,5])
+
+    # axs[0,1].plot(reduced_h[:,0])
+    # axs[3,1].plot(reduced_h[:,1])
+    # axs[4,1].plot(reduced_h[:,2])
+    # axs[5,1].plot(reduced_h[:,3])
+
+    # axs[0,2].plot(reduced_v[:,0])
+    # axs[3,2].plot(reduced_v[:,1])
+    # axs[4,2].plot(reduced_v[:,2])
+    # axs[5,2].plot(reduced_v[:,3])
+    # plt.show()
 
 
     # Undersampling
-    SPLIT = 0.2
+    SPLIT = 0.5
     h_idx = np.random.choice(range(data[0].shape[0]),
       int(data[0].shape[0] * SPLIT),
       replace=False)
@@ -74,81 +129,80 @@ def main(args):
 
     # Saving the data
     if save:
-      dumpfile = open("tmp/svr_models1.bin", "wb")
+      dumpfile = open("tmp/svr_models4.bin", "wb")
       pickle.dump(models, dumpfile)
       dumpfile.close()
   else:
     # Saving the data
-    loadfile = open("tmp/svr_models1.bin", "rb")
+    loadfile = open("tmp/svr_models4.bin", "rb")
     models = pickle.load(loadfile)
     loadfile.close()
 
     hmodel = models[0]
     vmodel = models[1]
 
-  #Reading Testing data
+  # Reading Testing data
   trackers = dict()
   br = libsolve.BagReader(testfile)
-  br.read("/loc/vive/trackers", libvsolve.trackerhandler, trackers)
+  br.read("/loc/vive/trackers", libnrvsolve.trackerhandler, trackers)
   # Reading light data
-  data = [np.empty([0,6]),
+  data = [np.empty([0,4]),
     np.empty([0,1]),
-    np.empty([0,6]),
+    np.empty([0,4]),
     np.empty([0,1])]
-  lr = libvsolve.LightReader(testfile, trackers)
+  lr = libnrvsolve.LightReader(testfile, trackers)
   lr.read("/loc/vive/light", data)
 
-  # Coordinate conversion
-  reduced_h = np.empty([0,4])
-  reduced_v = np.empty([0,4])
-  for i in range(0,data[0].shape[0]):
-    distance = np.sqrt(data[0][i,0]**2 + data[0][i,1]**2)
-    alpha_h = np.arctan(data[0][i,0] / data[0][i,2])
-    height = data[0][i,1]
-    Ry = np.matrix([[np.cos(alpha_h),0.0,np.sin(alpha_h)],
-      [0.0, 1.0, 0.0],
-      [-np.sin(alpha_h),0.0,np.cos(alpha_h)]])
-    R = np.asmatrix(np.eye(3))
-    cv2.Rodrigues(data[0][i,3:6],lRt)
-    new_R = Ry.transpose() * R
-    trAl = np.array([0,0,0])
-    cv2.Rodrigues(new_R,trAl)
-    orientation_x = trAl[0]
-    orientation_y = trAl[1]
-    orientation_z = trAl[2]
-    features = np.array([distance, orientation_x, orientation_y, orientation_z])
-    reduced_h = np.vstack((reduced_h, features))
+  # # Coordinate conversion
+  # reduced_h = np.empty([0,4])
+  # reduced_v = np.empty([0,4])
+  # for i in range(0,data[0].shape[0]):
+  #   distance = np.sqrt(data[0][i,0]**2 + data[0][i,1]**2)
+  #   alpha_h = np.arctan2(data[0][i,0] , data[0][i,2])
+  #   height = data[0][i,1]
+  #   Ry = np.matrix([[np.cos(alpha_h),0.0,np.sin(alpha_h)],
+  #     [0.0, 1.0, 0.0],
+  #     [-np.sin(alpha_h),0.0,np.cos(alpha_h)]])
+  #   R = cv2.Rodrigues(data[0][i,3:6])[0]
+  #   new_R = Ry * R
+  #   trAl = cv2.Rodrigues(new_R)[0]
+  #   orientation_x = trAl[0]
+  #   orientation_y = trAl[1]
+  #   orientation_z = trAl[2]
+  #   features = np.array([distance, orientation_x, orientation_y, orientation_z])
+  #   reduced_h = np.vstack((reduced_h, features))
 
-  for i in range(0,data[2].shape[0]):
-    distance = np.sqrt(data[2][i,0]**2 + data[2][i,1]**2)
-    alpha_h = np.arctan(data[2][i,1] / data[2][i,2])
-    height = data[2][i,0]
-    Ry = np.matrix([[np.cos(alpha_h),0.0,np.sin(alpha_h)],
-      [0.0, 1.0, 0.0],
-      [-np.sin(alpha_h),0.0,np.cos(alpha_h)]])
-    R = np.asmatrix(np.eye(3))
-    cv2.Rodrigues(data[2][i,3:6],lRt)
-    new_R = Ry.transpose() * R
-    trAl = np.array([0,0,0])
-    cv2.Rodrigues(new_R,trAl)
-    orientation_x = trAl[0]
-    orientation_y = trAl[1]
-    orientation_z = trAl[2]
-    features = np.array([distance, orientation_x, orientation_y, orientation_z])
-    reduced_v = np.vstack((reduced_v, features))
+  # for i in range(0,data[2].shape[0]):
+  #   distance = np.sqrt(data[2][i,0]**2 + data[2][i,1]**2)
+  #   alpha_h = np.arctan2(data[2][i,1] , data[2][i,2])
+  #   height = data[2][i,0]
+  #   Ry = np.matrix([[np.cos(alpha_h),0.0,np.sin(alpha_h)],
+  #     [0.0, 1.0, 0.0],
+  #     [-np.sin(alpha_h),0.0,np.cos(alpha_h)]])
+  #   R = cv2.Rodrigues(data[2][i,3:6])[0]
+  #   new_R = Ry * R
+  #   trAl = cv2.Rodrigues(new_R)[0]
+  #   orientation_x = trAl[0]
+  #   orientation_y = trAl[1]
+  #   orientation_z = trAl[2]
+  #   features = np.array([distance, orientation_x, orientation_y, orientation_z])
+  #   reduced_v = np.vstack((reduced_v, features))
 
   # Predict with test data
-  predicted_hdata = hmodel.predict(reduced_h)
-  predicted_vdata = vmodel.predict(reduced_v)
+  predicted_hdata = hmodel.predict(data[0])
+  predicted_vdata = vmodel.predict(data[2])
 
   # Plot
-  fig, axs = plt.subplots(1, 2, sharex = False, sharey= False)
+  fig, axs = plt.subplots(2, 2, sharex = False, sharey= False)
+  axs[0,0].plot(predicted_hdata)
+  axs[0,0].plot(data[1])
+  axs[0,1].plot(predicted_vdata)
+  axs[0,1].plot(data[3])
   hist, bin_edges = np.histogram(predicted_hdata - data[1], bins = 20)
-  axs[0].bar(bin_edges[:-1], hist, width = bin_edges[0] - bin_edges[1])
+  axs[1,0].bar(bin_edges[:-1], hist, width = bin_edges[0] - bin_edges[1])
   hist, bin_edges = np.histogram(predicted_vdata - data[3], bins = 20)
-  axs[1].bar(bin_edges[:-1], hist, width = bin_edges[0] - bin_edges[1])
+  axs[1,1].bar(bin_edges[:-1], hist, width = bin_edges[0] - bin_edges[1])
   plt.show()
-
 
   pass
 
